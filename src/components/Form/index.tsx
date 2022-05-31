2f41e3c5b6a998533637b55d8ac67265eb70f03f
Antônio Basílio <Antoniovsb25@gmail.com>
2022-05-31 07:38:56 -0300

    feat: second commit

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,6 +4,34 @@ import Graphic from "./components/Graphic";
 const Form = () => {
   const [loading, setLoading] = useState(false);
   const [actives, setActives] = useState({});
+  const [inputActive, setInputActive] = useState({
+    active: "",
+    initial_date: "",
+    final_date: "",
+  });
+
+  const postActivesName = async (e: any) => {
+    e.preventDefault();
+    const teste = { inputActive }
+    console.log("lastState", teste)
+    try {
+      const response = await fetch("http://localhost:4567/", {
+        method: "POST",
+        body: JSON.stringify(inputActive),
+        headers: { "content-type": "application/json" },
+      });
+      console.log("success!")
+      console.log("response", response)
+    } catch (error) {
+      console.log("something went wrong when fetching data", error);
+    }
+  };
+
+  const handleInput = (e: any) => {
+    const newData = { ...inputActive };
+    newData[e.target.id] = e.target.value;
+    setInputActive(newData);
+  };
 
   const getActivesContent = async () => {
     setLoading(true);
@@ -21,10 +49,37 @@ const Form = () => {
   return (
     <>
       <section>
+        <form onSubmit={postActivesName}>
+          <label htmlFor="active">Ação: </label>
+          <input
+            type="text"
+            id="active"
+            name="active"
+            value={inputActive.active}
+            onChange={(e) => handleInput(e)}
+          />
+          <label htmlFor="initial_date">Data Inicial: </label>
+          <input
+            type="date"
+            id="initial_date"
+            name="initial_date"
+            value={inputActive.initial_date}
+            onChange={(e) => handleInput(e)}
+          />
+          <label htmlFor="final_date">Data Final: </label>
+          <input
+            type="date"
+            id="final_date"
+            name="final_date"
+            value={inputActive.final_date}
+            onChange={(e) => handleInput(e)}
+          />
+          <button>POST DATA</button>
+        </form>
         <button type="button" onClick={getActivesContent}>
           Fetch Data
         </button>
-        <Graphic loading={loading} actives={actives}/>
+        <Graphic loading={loading} actives={actives} />
       </section>
     </>
   );
--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 const cors = require("cors");
 const express = require("express");
 const axios = require("axios");
+var bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 const API_KEY = "9QEY0OOLLBS1YIX5";
 const BASE_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=PETR4.SA&apikey=${API_KEY}`;
 const PORT = "4567";
 
+app.post("/", (req, res) => {
+    res.send(req.body)
+})
+
 app.get("/", async (req, res) => {
   const { data } = await axios(BASE_URL);
+  console.log(response)
   return res.json(data);
 });
 
